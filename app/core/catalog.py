53f37315093db1b59feeb7484207e6bc3e9c094b
pikvic <truepikvic@gmail.com>
2021-05-09 14:58:54 +1000

    Add async get with date param

--- app/core/catalog.py
+++ app/core/catalog.py
@@ -2,6 +2,7 @@ import json
 import httpx
 import pprint
 import asyncio
+import datetime
 from helpers import ConvertToWRS
 
 
@@ -9,30 +10,46 @@ async def get_item(url):
     async with httpx.AsyncClient() as client:
         print(f"Getting item from {url}")
         response = await client.get(url)
-        print(f"Got item from {url}")
-        data = json.loads(response.text)
+        print(f"Got item from {url} Code: {response.status_code}")
+        data = None
+        if response.status_code == 200:
+            data = json.loads(response.text)
         return data
    
-async def get_items(path, row, limit=1):
-    url = f"https://landsat-stac.s3.amazonaws.com/landsat-8-l1/{path:03d}/{row:03d}/catalog.json"
-    with httpx.Client() as client:
-        response = client.get('https://www.example.com/')
-    response = httpx.get(url)
-    data = json.loads(response.text)
-    base_url = url.replace('catalog.json', '')
-    links = [f"{base_url}{link['href']}" for link in data['links'] if link['rel'] == 'item']
+def is_date_between(href, dates):
+    href = href.split('/')[0]
+    href_date = datetime.datetime.strptime(href, "%Y-%m-%d").date()
+    return dates[0] <= href_date <= dates[1]
+
+async def get_items(wrs_list, dates=None, limit=1):
+    if not dates:
+        delta = datetime.timedelta(days=30)
+        today = datetime.datetime.now()
+        dates = [(today - delta).date(), today.date()]
+    
+    base_url = "https://landsat-stac.s3.amazonaws.com/landsat-8-l1"
+    links = [f"{base_url}/{wrs['path']:03d}/{wrs['row']:03d}/catalog.json" for wrs in wrs_list]
+    tasks = [get_item(link) for link in links[:limit]]
+    catalogs = await asyncio.gather(*tasks)
+    catalogs = [catalog for catalog in catalogs if catalog]
+
+    links = []
+    for catalog in catalogs:
+        base_url = [link['href'] for link in catalog['links'] if link['rel'] == 'self'][0]
+        base_url = base_url.replace('catalog.json', '')
+        links += [f"{base_url}{link['href']}" for link in catalog['links'] if link['rel'] == 'item' and is_date_between(link['href'], dates)]
 
     tasks = [get_item(link) for link in links[:limit]]
     result = await asyncio.gather(*tasks)
     return result
     
-async def main():
-    result = await get_items(17, 110)
-    pprint.pprint(result)
+async def main(wrs_list):
+    result = await get_items(wrs_list, limit=1000)
+
+    pprint.pprint(len(result))
 
 if __name__ == "__main__":
     wrs = ConvertToWRS()
     lonlat = (130., 40., 135., 45.)
-    result = wrs.get_wrs_list(*lonlat)
-    print(result)
-    #asyncio.run(main())
+    wrs_list = wrs.get_wrs_list(*lonlat)
+    asyncio.run(main(wrs_list))
